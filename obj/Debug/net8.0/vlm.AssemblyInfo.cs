51f5d85491c203167e8b027d593cc85ece0c5e86
Chanti <vasarla.chanti@capgemini.com>
2025-04-14 12:57:48 +0530

    test

--- obj/Debug/net8.0/vlm.AssemblyInfo.cs
+++ obj/Debug/net8.0/vlm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vlm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b393ed9da04258120958ac374e2f23d2552a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("vlm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vlm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
